49ff8cba08a604feae9ed44aa60e63cfb1b93a27
Alex Foster <alex.88.foster@gmail.com>
2023-08-28 11:24:16 +1000

    Add player

---
+++ src/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+@export var speed: int
+
+
+func _physics_process(_delta):
+	var input_direction = get_input_direction()
+	if input_direction:
+		velocity = input_direction * speed
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
+
+
+func get_input_direction() -> Vector2:
+	var input_direction_x = Input.get_axis("move_left", "move_right")
+	var input_direction_y = Input.get_axis("move_up", "move_down")
+	return Vector2(input_direction_x, input_direction_y)

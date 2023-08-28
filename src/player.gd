f754f4e79c0a287ac18c31e73bc3693484007b2b
justinyotabyte <116635799+justinyotabyte@users.noreply.github.com>
2023-08-28 23:03:55 +1000

    Added random wall removal

--- src/player.gd
+++ src/player.gd
@@ -5,17 +5,17 @@ extends CharacterBody2D
 
 
 func _physics_process(_delta):
-	var input_direction = get_input_direction()
-	if input_direction:
-		velocity = input_direction * speed
-		$AnimationPlayer.play("walk_down")
-	else:
-		velocity = Vector2.ZERO
-		$AnimationPlayer.play("idle")
-	move_and_slide()
+    var input_direction = get_input_direction()
+    if input_direction:
+        velocity = input_direction * speed
+        $AnimationPlayer.play("walk_down")
+    else:
+        velocity = Vector2.ZERO
+        $AnimationPlayer.play("idle")
+    move_and_slide()
 
 
 func get_input_direction() -> Vector2:
-	var input_direction_x = Input.get_axis("move_left", "move_right")
-	var input_direction_y = Input.get_axis("move_up", "move_down")
-	return Vector2(input_direction_x, input_direction_y)
+    var input_direction_x = Input.get_axis("move_left", "move_right")
+    var input_direction_y = Input.get_axis("move_up", "move_down")
+    return Vector2(input_direction_x, input_direction_y)
--- src/scripts/maze.gd
+++ src/scripts/maze.gd
@@ -8,134 +8,169 @@ var cells = []
 
 
 class MazeNode:
-	var x : int
-	var y : int
-	var visited: bool = false
-	var east_path: bool = false
-	var south_path: bool = false
-	
-	func _init(x: int, y: int) -> void:
-		self.x = x
-		self.y = y
-		
-	func _to_string():
-		return "[x: " + str(self.x) + ", y: " + str(self.y) + "], east_path: " + str(self.east_path) + "|south_path: " + str(self.south_path)
+    var x : int
+    var y : int
+    var visited: bool = false
+    var east_path: bool = false
+    var south_path: bool = false
+    
+    func _init(x: int, y: int) -> void:
+        self.x = x
+        self.y = y
+        
+    func _to_string():
+        return "[x: " + str(self.x) + ", y: " + str(self.y) + "], east_path: " + str(self.east_path) + "|south_path: " + str(self.south_path)
 
 class MazeNodeStack:
-	var l: Array[MazeNode] = []
-	
-	func push(node: MazeNode) -> void:
-		l.append(node)
-		
-	func peek() -> MazeNode:
-		return l[-1]
-		
-	func pop() -> MazeNode:
-		return l.pop_back()
-		
-	func _to_string():
-		return str(self.l)
-		
-	func size():
-		return l.size()
-		
-	func empty():
-		return self.size() == 0
+    var l: Array[MazeNode] = []
+    
+    func push(node: MazeNode) -> void:
+        l.append(node)
+        
+    func peek() -> MazeNode:
+        return l[-1]
+        
+    func pop() -> MazeNode:
+        return l.pop_back()
+        
+    func _to_string():
+        return str(self.l)
+        
+    func size():
+        return l.size()
+        
+    func empty():
+        return self.size() == 0
 
 func initialize_grid():
-	var atlas_coords = Vector2i(3, 3)
-	
-	# Draw north and west walls.
-	
-	for y in range((grid_size * cell_size) + 1):
-		$TileMap.set_cell(0, Vector2i(-1, y - 1), 0, atlas_coords)
-		cells.append(Vector2i(-1, y - 1))
-	for x in range((grid_size * cell_size) + 1):
-		$TileMap.set_cell(0, Vector2i(x - 1, -1), 0, atlas_coords)
-		cells.append(Vector2i(x - 1, -1))        
-	
-	for y in range(grid_size):
-		grid.append([])
-		for x in range(grid_size):
-			grid[y].append(MazeNode.new(x, y))
-			for y_cell in range(cell_size):
-				for x_cell in range(cell_size):
-					if not x_cell and not y_cell:
-						continue
-					$TileMap.set_cell(0, Vector2i((x * cell_size) + y_cell, (y * cell_size) + x_cell), 0, atlas_coords)
+    var atlas_coords = Vector2i(3, 3)
+    
+    # Draw north and west walls.
+    
+    for y in range((grid_size * cell_size) + 1):
+        $TileMap.set_cell(0, Vector2i(-1, y - 1), 0, atlas_coords)
+        cells.append(Vector2i(-1, y - 1))
+    for x in range((grid_size * cell_size) + 1):
+        $TileMap.set_cell(0, Vector2i(x - 1, -1), 0, atlas_coords)
+        cells.append(Vector2i(x - 1, -1))        
+    
+    for y in range(grid_size):
+        grid.append([])
+        for x in range(grid_size):
+            grid[y].append(MazeNode.new(x, y))
+            for y_cell in range(cell_size):
+                for x_cell in range(cell_size):
+                    if not x_cell and not y_cell:
+                        continue
+                    $TileMap.set_cell(0, Vector2i((x * cell_size) + y_cell, (y * cell_size) + x_cell), 0, atlas_coords)
 
+func get_random_exit() -> Vector2i:
+    var options = []
+    
+    for i in range(grid_size):
+        # All northern wall.
+        if $TileMap.get_cell_source_id(0, Vector2i(i, 0)) == -1:
+            options.append(Vector2i(i, -1))
+        
+        # All eastern wall.
+        if $TileMap.get_cell_source_id(0, Vector2i((grid_size * cell_size) - 2, i)) == -1:
+            options.append(Vector2i((grid_size * cell_size) - 1, i))
+        
+        # All southern wall.
+        if $TileMap.get_cell_source_id(0, Vector2i(i, (grid_size * cell_size) - 2)) == -1:
+            options.append(Vector2i(i, (grid_size * cell_size) - 1))
+        
+        # All western wall.
+        if $TileMap.get_cell_source_id(0, Vector2i(0, i)) == -1:
+            options.append(Vector2i(-1, i))
+        
+    return options[randi() % options.size()]
   
 func draw_maze(tilemap: TileMap):
-	var source_id = -1
-	for y in range(grid.size()):
-		for x in range(grid[y].size()):
-			var current_node = grid[y][x];
-			if current_node.east_path:
-				var coords = Vector2i((x * cell_size) + 1, y * cell_size)
-				tilemap.set_cell(0, coords, source_id)
-			
-			if current_node.south_path:
-				var coords = Vector2i(x * cell_size , (y * cell_size) + 1)
-				tilemap.set_cell(0, coords, source_id)  
-				
-	$TileMap.set_cells_terrain_connect(0, $TileMap.get_used_cells(0), 0, 0, true)     
+    var source_id = -1
+    for y in range(grid.size()):
+        for x in range(grid[y].size()):
+            var current_node = grid[y][x];
+            if current_node.east_path:
+                var coords = Vector2i((x * cell_size) + 1, y * cell_size)
+                tilemap.set_cell(0, coords, source_id)
+            
+            if current_node.south_path:
+                var coords = Vector2i(x * cell_size , (y * cell_size) + 1)
+                tilemap.set_cell(0, coords, source_id)  
+                
 
-				
+                
 func recursive_backtracker():
-	var stack = MazeNodeStack.new()
-	stack.push(grid[0][0])
-	stack.peek().visited = true
-	
-	while not stack.empty():
-		var current_node = stack.peek()
-		var neighbours: Array = []
-		
-		# North
-		if current_node.y - 1 >= 0 and not grid[current_node.y - 1][current_node.x].visited:
-			neighbours.append([0, grid[current_node.y - 1][current_node.x]])
+    var stack = MazeNodeStack.new()
+    stack.push(grid[0][0])
+    stack.peek().visited = true
+    
+    while not stack.empty():
+        var current_node = stack.peek()
+        var neighbours: Array = []
+        
+        # North
+        if current_node.y - 1 >= 0 and not grid[current_node.y - 1][current_node.x].visited:
+            neighbours.append([0, grid[current_node.y - 1][current_node.x]])
 
-		# East
-		if current_node.x + 1 < grid.size() and not grid[current_node.y][current_node.x + 1].visited:
-			neighbours.append([1, grid[current_node.y][current_node.x + 1]])
-			
-		# South
-		if current_node.y + 1 < grid.size() and not grid[current_node.y + 1][current_node.x].visited:
-			neighbours.append([2, grid[current_node.y + 1][current_node.x]])
-			
-		# West
-		if current_node.x - 1 >= 0 and not grid[current_node.y][current_node.x - 1].visited:
-			neighbours.append([3, grid[current_node.y][current_node.x - 1]])
-		
-		if neighbours.is_empty():
-			stack.pop()
-		else:
-			var neighbour = neighbours[randi() % neighbours.size()]
-			if neighbour[0] == 0:
-				neighbour[1].south_path = true
-			elif neighbour[0] == 1:
-				current_node.east_path = true
-			elif neighbour[0] == 2:
-				current_node.south_path = true
-			elif neighbour[0] == 3:
-				neighbour[1].east_path = true
-				
-			stack.push(neighbour[1])
-			neighbour[1].visited = true
+        # East
+        if current_node.x + 1 < grid.size() and not grid[current_node.y][current_node.x + 1].visited:
+            neighbours.append([1, grid[current_node.y][current_node.x + 1]])
+            
+        # South
+        if current_node.y + 1 < grid.size() and not grid[current_node.y + 1][current_node.x].visited:
+            neighbours.append([2, grid[current_node.y + 1][current_node.x]])
+            
+        # West
+        if current_node.x - 1 >= 0 and not grid[current_node.y][current_node.x - 1].visited:
+            neighbours.append([3, grid[current_node.y][current_node.x - 1]])
+        
+        if neighbours.is_empty():
+            stack.pop()
+        else:
+            var neighbour = neighbours[randi() % neighbours.size()]
+            if neighbour[0] == 0:
+                neighbour[1].south_path = true
+            elif neighbour[0] == 1:
+                current_node.east_path = true
+            elif neighbour[0] == 2:
+                current_node.south_path = true
+            elif neighbour[0] == 3:
+                neighbour[1].east_path = true
+                
+            stack.push(neighbour[1])
+            neighbour[1].visited = true
 
-		
-			
+        
+func remove_wall_exit():
+    var cell = get_random_exit()
+    $TileMap.set_cell(0, cell, -1, Vector2i(3,3))
+    
+#    $TileMap2.set_cell(0, cell, 0, Vector2i(0,0))
+          
 
-		
+        
 func _ready():
-	rng.seed = 0
-	
-	initialize_grid()
+    rng.seed = 0
+    
+    initialize_grid()
 
 #    grid[0][0].east_path = true
 #    var coords = Vector2i(0, 0)
 #    $TileMap.set_cell(0, coords, 0, Vector2i(3,3))
 #    $TileMap.set_cell(0, coords, 0, Vector2i(3,3))
-	recursive_backtracker()
+    recursive_backtracker()
 #    initialize_grid()
-	draw_maze($TileMap)
+    draw_maze($TileMap)
+    remove_wall_exit()
+#    var my_timer = Timer.new()
+    $TileMap.set_cells_terrain_connect(0, $TileMap.get_used_cells(0), 0, 0, true)
+    
+#    my_timer.wait_time = 1.0  # Set the wait time to 1 second
+#    my_timer.autostart = true  # Set to start automatically
+#
+#    my_timer.connect("timeout", remove_wall_exit)
+#
+#    add_child(my_timer)
 

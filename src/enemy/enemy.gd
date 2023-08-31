1c6350cab8eb97357fc9ce6f759b266f2dc8183c
Alex Foster <alex.88.foster@gmail.com>
2023-08-31 10:24:06 +1000

    Change enemy from StaticBody2D to CharacterBody2D

--- src/enemy/enemy.gd
+++ src/enemy/enemy.gd
@@ -1,14 +1,12 @@
-extends StaticBody2D
+extends Actor
 
 
-const SPEED = 5
+const SPEED = 240
 const SILENCE_DISTANCE = 1000
 const SILENCE = -30
-var move_direction = Vector2()
+var direction = Vector2()
 var rng = RandomNumberGenerator.new()
 var player: CharacterBody2D = null
-
-@onready var animation_tree: AnimationTree = $AnimationTree
 
 
 func get_random_direction() -> Vector2i:
@@ -33,32 +31,25 @@ func control_laugh():
 func _process(_delta):
 	var current_position = position
 	control_laugh()
-	move_and_collide(move_direction * SPEED)
+	velocity = direction * SPEED
+	move_and_slide()
 	if current_position == position:
-		move_direction = get_random_direction()
-	set_animation(move_direction)
+		direction = get_random_direction()
+	set_animation(velocity)
+
 
 func _ready():
-	move_direction = get_random_direction()
+	direction = get_random_direction()
 	$AudioStreamPlayer2D.play()
 	player = get_tree().get_nodes_in_group("player")[0]
 
 
 func _on_bounce_zone_body_entered(_body):
-	var current_direction = move_direction
-	while current_direction == move_direction:
-		move_direction = get_random_direction()
+	var current_direction = direction
+	while current_direction == direction:
+		direction = get_random_direction()
 
 
 func _on_audio_stream_player_2d_finished():
 	$AudioStreamPlayer2D.stream_paused = false
 	$AudioStreamPlayer2D.play()
-
-
-func set_animation(direction):
-	if direction:
-		animation_tree.get("parameters/playback").travel("walk")
-		animation_tree.set("parameters/idle/blend_position", direction)
-		animation_tree.set("parameters/walk/blend_position", direction)
-	else:
-		animation_tree.get("parameters/playback").travel("idle")
--- src/player/player.gd
+++ src/player/player.gd
@@ -1,35 +1,20 @@
-extends CharacterBody2D
+extends Actor
 
 
 @export var speed: int
-
-@onready var animation_tree: AnimationTree = $AnimationTree
 
 
 func _physics_process(_delta):
 	var input_direction = get_input_direction()
 	if input_direction:
 		velocity = input_direction * speed
-		animation_tree.get("parameters/playback").travel("walk")
-		animation_tree.set("parameters/idle/blend_position", input_direction)
-		animation_tree.set("parameters/walk/blend_position", input_direction)
 	else:
 		velocity = Vector2.ZERO
-		animation_tree.get("parameters/playback").travel("idle")
 	move_and_slide()
-	set_animation(input_direction)
+	set_animation(velocity)
 
 
 func get_input_direction() -> Vector2:
 	var input_direction_x = Input.get_axis("move_left", "move_right")
 	var input_direction_y = Input.get_axis("move_up", "move_down")
 	return Vector2(input_direction_x, input_direction_y)
-
-
-func set_animation(direction):
-	if direction:
-		animation_tree.get("parameters/playback").travel("walk")
-		animation_tree.set("parameters/idle/blend_position", direction)
-		animation_tree.set("parameters/walk/blend_position", direction)
-	else:
-		animation_tree.get("parameters/playback").travel("idle")
